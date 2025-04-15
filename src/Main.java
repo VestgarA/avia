eafec3f7ce3a4289f229e2f887161f894d26ecb6
vestgar <sid06@bk.ru>
2025-04-15 11:57:56 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+       int ticketPrice = 15600;
+       int bonusPercentage = 20;
+       int bonuses = ticketPrice / bonusPercentage;
+        System.out.println(bonuses);
+    }
+}
